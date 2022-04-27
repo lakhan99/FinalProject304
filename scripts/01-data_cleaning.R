36d8cbcdf6372dfe46799e5405e0c3a19b5dd5d0
Arsh Lakhanpal <arsh.lakhanpal@mail.utoronto.ca>
2022-04-27 01:51:20 -0400

    Push before final edit

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -15,19 +15,14 @@
 # Use R Projects, not setwd().
 library(haven)
 library(tidyverse)
-# Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
+
+#Download the raw data from the repository
+#This data can be downloaded from: https://archive.ics.uci.edu/ml/datasets/Estimation+of+obesity+levels+based+on+eating+habits+and+physical+condition+
+
+# Read in the raw data.
+ObesityDataSet_raw_and_data_sinthetic <- read_csv("~/starter_folder-main/inputs/data/ObesityDataSet_raw_and_data_sinthetic_final.csv")
+obesity_data <- ObesityDataSet_raw_and_data_sinthetic
 
 #### What's next? ####
 
